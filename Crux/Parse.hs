3ece924f17d9040b121c86809098c2e1bdf83d0c
Andy Friesen <andy.friesen@gmail.com>
2014-10-18 15:03:57 -0700

    Checkpoint.

--- Crux/Parse.hs
+++ Crux/Parse.hs
@@ -5,25 +5,37 @@ import Crux.Tokens
 import Crux.AST
 import qualified Text.Parsec as P
 import Control.Monad.Identity (Identity)
+import Data.Text (Text)
 
-token :: P.ParsecT [Token] u Identity Token
+type Parser = P.ParsecT [Token] () Identity
+
+token :: Parser Token
 token = P.tokenPrim showTok nextPos testTok
   where
     showTok = show
     nextPos pos _ _ = pos
     testTok = Just
 
-printStatement :: P.ParsecT [Token] u Identity Expression
-printStatement = do
-    TIdentifier i <- token
-    if i == "print" then do
-        expr <- token
-        case expr of
-            TString value -> return $ EPrint $ LString value
-            TInteger value -> return $ EPrint $ LInteger value
-            _ -> fail "Expected int or string"
-    else fail "Expected 'print'"
+identifier :: Text -> Parser Token
+identifier name = P.tokenPrim showTok nextPos testTok
+  where
+    showTok = show
+    nextPos pos _ _ = pos
+    testTok tok = case tok of
+        TIdentifier t | t == name -> Just tok
+        _ -> Nothing
 
+printStatement :: Parser Expression
+printStatement = do
+    identifier "print"
+    expr <- token
+    case expr of
+        TString value -> return $ EPrint $ LString value
+        TInteger value -> return $ EPrint $ LInteger value
+        _ -> fail "Expected int or string"
+
+document :: Parser Expression
 document = printStatement
 
+parse :: P.SourceName -> [Token] -> Either P.ParseError Expression
 parse fileName tokens = P.runParser document () fileName tokens
--- Main.hs
+++ Main.hs
@@ -5,19 +5,39 @@ import System.Environment (getArgs)
 
 import Crux.Lex
 import Crux.Parse
+import Crux.Gen
+
+import LLVM.General.Context
+import LLVM.General.Module
+import Control.Monad (void)
+import Control.Monad.Except (runExceptT)
+import Control.Monad.Error
+
+help = putStrLn "Pass a single filename as an argument"
 
 main :: IO ()
 main = do
     args <- getArgs
+
     case args of
+        [] -> help
+        (_:_:_) -> help
         [fn] -> do
             l <- Crux.Lex.lex fn
-            print l
-
             case l of
                 Left err -> putStrLn $ "Lex error: " ++ show err
                 Right l' -> do
+                    putStrLn "Lex ok"
                     let p = Crux.Parse.parse fn l'
-                    print p
-
-        _ -> return ()
+                    case p of
+                        Left err -> putStrLn $ "Parse error: " ++ show err
+                        Right p' -> do
+                            putStrLn "Parse OK"
+                            m <- withContext $ \context ->
+                                runExceptT $ withModuleFromAST context (Crux.Gen.gen "main" [p']) $ \mod -> do
+                                    llstr <- moduleLLVMAssembly mod
+                                    putStrLn llstr
+                                    return mod
+                            case m of
+                                Left err -> putStrLn $ "Codegen error: " ++ err
+                                Right _ -> putStrLn "Done"

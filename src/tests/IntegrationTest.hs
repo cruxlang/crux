f421620a48edbe537be57c2674a639ee13734c49
Andy Friesen <andy.friesen@gmail.com>
2015-06-01 20:45:38 -0700

    Integration test.

---
+++ src/tests/IntegrationTest.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module IntegrationTest (run, tests) where
+
+import           Control.Monad  (forM)
+import           Crux.JSGen
+import qualified Crux.JSTree    as JSTree
+import           Crux.Lex
+import           Crux.Parse
+import qualified Crux.Typecheck as Typecheck
+import           Data.Text      (Text)
+import qualified Data.Text      as T
+import qualified Data.Text.IO   as T
+import           System.Process (readProcess)
+import           Test.HUnit
+import qualified Data.Text as T
+-- import           Text.Show.Pretty   (ppShow)
+import           Data.Monoid    ((<>))
+
+run :: Text -> IO (Either String Text)
+run src = do
+    let fn = "<string>"
+    let l = Crux.Lex.lexSource fn src
+    case l of
+        Left err ->
+            return $ Left $ "Lex error: " <> show err
+        Right l' -> do
+            p <- Crux.Parse.parse fn l'
+            case p of
+                Left err -> return $ Left $ "Parse error: " ++ show err
+                Right p' -> do
+                    typetree <- Typecheck.run p'
+                    typetree' <- forM typetree Typecheck.flattenDecl
+                    T.writeFile "temp.js" $ JSTree.renderDocument (concatMap generateDecl typetree')
+
+                    fmap (Right . T.pack) $ readProcess "node" ["temp.js"] ""
+
+testHelloWorld = do
+    result <- run $ T.unlines
+        [ "let _ = print \"Hello, World!\";"
+        ]
+    assertEqual "" (Right "Hello, World!\n") result
+
+tests :: Test
+tests = TestList
+    [ TestLabel "testHelloWorld" $ TestCase testHelloWorld
+    ]
--- src/tests/Tests.hs
+++ src/tests/Tests.hs
@@ -2,8 +2,16 @@
 module Main where
 
 import qualified ParseTest
+import qualified IntegrationTest
 import Test.HUnit
+import System.Exit (exitWith, ExitCode (..))
 
-main = runTestTT $ TestList
-    [ ParseTest.tests
-    ]
+main :: IO Counts
+main = do
+    testCounts <- runTestTT $ TestList
+        [ ParseTest.tests
+        , IntegrationTest.tests
+        ]
+    case failures testCounts of
+        0 -> exitWith ExitSuccess
+        _ -> exitWith $ ExitFailure 1

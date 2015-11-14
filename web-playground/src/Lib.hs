5cdabfaeefc5830db866bc08597a0e8d466660ef
Andy Friesen <andy.friesen@gmail.com>
2015-11-14 09:12:48 -0800

    Checkpoint.  web-playground compiles.

--- web-playground/src/Lib.hs
+++ web-playground/src/Lib.hs
@@ -4,19 +4,26 @@ module Lib
 
 import qualified Crux.Backend.JS as JS
 import qualified Crux.Gen        as Gen
-import           Crux.Module     (loadProgramFromFile)
-import           GHCJS.DOM       (runWebGUI)
+import           Crux.Module     (loadProgramFromSource)
+import           GHCJS.DOM       (runWebGUI, webViewGetDomDocument)
+import GHCJS.DOM.Document (documentGetBody)
+import GHCJS.DOM.Element (elementQuerySelector, elementOnclick)
+import GHCJS.DOM.HTMLElement (htmlElementGetInnerText, castToHTMLElement)
+import Control.Monad.Trans (liftIO)
 
 run :: IO ()
 run = runWebGUI $ \webView -> do
     Just doc <- webViewGetDomDocument webView
     Just body <- documentGetBody doc
 
-    Just textArea <- elementQuerySelector body "#textArea"
-    Just runButton <- elementQuerySelector body "#run"
+    Just textArea <- (fmap castToHTMLElement) <$> elementQuerySelector body "#textArea"
+    Just runButton <- (fmap castToHTMLElement) <$> elementQuerySelector body "#run"
 
-    elementOnClick runButton $ \_ _ -> do
-        content <- HtmlElementGetInnerText textArea
-        undefined
+    elementOnclick runButton $ liftIO $ do
+        content <- htmlElementGetInnerText textArea
+        program <- loadProgramFromSource "<playground>" content
+        program' <- Gen.generateProgram program
+        let js = JS.generateJS program'
+        print js
 
     putStrLn "Setup done!"

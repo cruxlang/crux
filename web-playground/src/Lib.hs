1a0ad232f0f908018fbb3bc890210afd75db8992
Andy Friesen <andy.friesen@gmail.com>
2015-12-23 21:05:49 -0800

    Update README.md and the playground.

--- web-playground/src/Lib.hs
+++ web-playground/src/Lib.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Lib
@@ -7,19 +7,17 @@ module Lib
 
 import           Control.Monad                 (void)
 import           Control.Monad.Trans           (liftIO)
-import Control.Exception (try)
-import GHCJS.Foreign
+import qualified Data.HashMap.Strict as HashMap
+import           Control.Exception (try)
+import           GHCJS.Foreign
 import           Crux.AST                      (ModuleName (..),
                                                 ModuleSegment (..))
 import qualified Crux.AST                      as AST
-import qualified Crux.Backend.JS               as JS
-import Crux.Typecheck.Types (UnificationError, errorToString)
-import GHCJS.Types (JSRef, JSString)
+import qualified Crux.JSBackend                as JS
+import           Crux.Typecheck.Types          (UnificationError, errorToString)
+import           GHCJS.Types                   (JSRef, JSString)
 import qualified Crux.Gen                      as Gen
-import           Crux.Module                   (loadModule,
-                                                loadPreludeFromSource,
-                                                loadProgramFromSource,
-                                                newMemoryLoader)
+import           Crux.Module                   (newMemoryLoader, loadProgram)
 import           Data.FileEmbed                (embedFile)
 import           Data.IORef                    (newIORef, readIORef)
 import           Data.String                   (fromString)
@@ -44,20 +42,14 @@ import           GHCJS.Foreign                 (toJSString)
 
 compile source = do
     let mainModuleName = "Main"
-        loader = newMemoryLoader mainModuleName source
         preludeSource = decodeUtf8 $(embedFile "../lib/Prelude.cx")
 
-    prelude <- loadPreludeFromSource preludeSource
-    loadedModules <- newIORef [(fromString "Prelude", prelude)]
+        loader = newMemoryLoader $ HashMap.fromList
+            [ ("Prelude", preludeSource)
+            , ("Main", source)
+            ]
 
-    mainModule <- loadModule loader loadedModules (fromString "Main")
-
-    otherModules <- readIORef loadedModules
-    let program = AST.Program
-            { AST.pMainModule = mainModule
-            , AST.pOtherModules = otherModules
-            }
-
+    program <- loadProgram loader "Main"
     program' <- Gen.generateProgram program
     return $ JS.generateJS program'
 
@@ -72,10 +64,10 @@ run = runWebGUI $ \_webView -> do
             r <- try $ f arg
             case r of
                 Right code ->
-                    setProp "result" code retObj
+                    setProp ("result" :: String) code retObj
                 Left err -> do
                     s <- toJSString <$> errorToString err
-                    setProp "error" s retObj
+                    setProp ("error" :: String) s retObj
 
     callback <- syncCallback2 NeverRetain False (returnViaArgument compileJS)
     js_setTheFunction callback

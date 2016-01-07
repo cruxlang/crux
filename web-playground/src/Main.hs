a9e7ce809b9abcb2034b08dc375f5374b1314d3d
Chad Austin <chad@chadaustin.me>
2016-01-06 23:29:24 -0800

    update to newer ghcjs snapshot which lets us simplify the ffi a bit

--- web-playground/src/Main.hs
+++ web-playground/src/Main.hs
@@ -11,16 +11,14 @@ import Crux.Typecheck.Types (errorToString)
 import Data.FileEmbed (embedFile)
 import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8)
-import GHCJS.Concurrent (OnBlocked(ThrowWouldBlock))
-import GHCJS.Foreign.Callback (Callback, syncCallback2)
+import GHCJS.Foreign.Callback (Callback, syncCallback1')
 import Data.JSString.Text (textToJSString, textFromJSString)
 import GHCJS.Types (JSVal, JSString, jsval)
-import JavaScript.Object (setProp)
+import qualified JavaScript.Object as Object
 import qualified Crux.Gen as Gen
 import qualified Crux.JSBackend as JS
 import qualified Data.HashMap.Strict as HashMap
 import GHCJS.Marshal.Pure (pFromJSVal, pToJSVal)
-import Unsafe.Coerce (unsafeCoerce)
 
 compile :: Text -> IO Text
 compile source = do
@@ -51,11 +49,13 @@ main :: IO ()
 main = do
     -- TODO: we can return the result object instead of taking it as an argument
     -- when we upgrade ghcjs.  we would use syncCallback1' in that case.
-    exportCallback "compileCrux" $ syncCallback2 ThrowWouldBlock $ \source resultObject -> do
+    exportCallback "compileCrux" $ syncCallback1' $ \source -> do
+        resultObject <- Object.create
         r <- try $ compileJS $ pFromJSVal source
         case r of
             Right code ->
-                setProp "result" (pToJSVal code) (unsafeCoerce resultObject)
+                Object.setProp "result" (pToJSVal code) resultObject
             Left err -> do
                 s <- errorToString err
-                setProp "error" (pToJSVal s) (unsafeCoerce resultObject)
+                Object.setProp "error" (pToJSVal s) resultObject
+        return $ jsval resultObject

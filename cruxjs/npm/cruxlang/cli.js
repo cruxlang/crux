078064f26129ee6b8ccad4dc76db4c431df6971a
Chad Austin <chad@chadaustin.me>
2016-01-07 23:51:26 -0800

    add an npm package for cruxlang

---
+++ cruxjs/npm/cruxlang/cli.js
@@ -0,0 +1,14 @@
+"use strict";
+const crux = require('./src/crux.js');
+const fs = require('fs');
+
+// TODO: error checking
+const sourceFile = process.argv[2];
+let sourceContents = fs.readFileSync(sourceFile, 'utf8');
+let results = crux.compileCrux(sourceContents);
+if (results.result) {
+  process.stdout.write(results.result);
+} else {
+  process.stderr.write(results.error);
+  process.exit(1);
+}

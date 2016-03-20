1cf40fa1eed4b44a58e3a5e0b251883f293b0f21
Chad Austin <chad@chadaustin.me>
2016-03-20 09:16:18 -0700

    work towards making this thing a service

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.sql.*;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Map;
@@ -7,54 +6,44 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import static spark.Spark.*;
-import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
 import static spark.Spark.get;
 
-import com.heroku.sdk.jdbc.DatabaseUrl;
+import com.google.javascript.jscomp.CompilationLevel;
+import com.google.javascript.jscomp.Compiler;
+import com.google.javascript.jscomp.CompilerOptions;
+import com.google.javascript.jscomp.Result;
+import com.google.javascript.jscomp.SourceFile;
 
 public class Main {
 
   public static void main(String[] args) {
-
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
     get("/hello", (req, res) -> "Hello World");
 
     get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("message", "Hello World!");
 
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
-
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
+        return new ModelAndView(attributes, "index.ftl");
     }, new FreeMarkerEngine());
 
+    post("/compile", (request, response) -> {
+        Compiler compiler = new Compiler();
+        CompilerOptions options = new CompilerOptions();
+        ArrayList<SourceFile> source = new ArrayList<>();
+        source.add(SourceFile.fromCode("foo.js", "function foobar(x) {}"));
+        Result result = compiler.compile(null, source, options);
+        if (!result.success) {
+            // result.warnings
+            // result.errors
+        }
+
+        response.status(200);
+        response.body(compiler.toSource());
+    });
   }
 
 }

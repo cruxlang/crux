8cb5d42588fc40c0e807bf2fd9be8cdc7d0fb4ab
Chad Austin <chad@chadaustin.me>
2016-03-20 15:08:48 -0500

    compilation works now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import static spark.Spark.*;
+import spark.ResponseTransformer;
 import spark.ModelAndView;
 import static spark.Spark.get;
 
@@ -16,40 +17,63 @@ import com.google.javascript.jscomp.Result;
 import com.google.javascript.jscomp.SourceFile;
 import com.google.javascript.jscomp.VariableRenamingPolicy;
 import com.google.javascript.jscomp.PropertyRenamingPolicy;
+import com.google.gson.Gson;
+
+class CompileRequest {
+    String filename;
+    String source;
+}
+
+class CompileResponse {
+    String source;
+}
 
 public class Main {
+    public static ResponseTransformer json() {
+        return (object) -> new Gson().toJson(object);
+    }
 
-  public static void main(String[] args) {
-    port(Integer.valueOf(System.getenv("PORT")));
-    //staticFileLocation("/public");
+    public static void main(String[] args) {
+        port(Integer.valueOf(System.getenv("PORT")));
+        //staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> "Hello World");
 
-    post("/compile", (request, response) -> {
-        Compiler compiler = new Compiler();
-        CompilerOptions options = new CompilerOptions();
-        CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel(options);
+        post("/compile", (req, res) -> {
+            String body = req.body();
+            CompileRequest request = new Gson().fromJson(body, CompileRequest.class);
+            if (null == request.filename) {
+                request.filename = "<unspecified>.js";
+            }
 
-        // This entire project exists because of the following line of code.  :|
-        // See [TODO: find github issue]
-        options.setRenamingPolicy(VariableRenamingPolicy.OFF, PropertyRenamingPolicy.OFF);
-        options.prettyPrint = true;
+            Compiler compiler = new Compiler();
+            CompilerOptions options = new CompilerOptions();
+            CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel(options);
 
-        //List<SourceFile> builtinExterns = getBuiltinExterns(options.getEnvironment());
-        ArrayList<SourceFile> externs = new ArrayList<>();
-        ArrayList<SourceFile> source = new ArrayList<>();
-        source.add(SourceFile.fromCode("foo.js", "function foobar(x) {}"));
-        Result result = compiler.compile(externs, source, options);
-        if (!result.success) {
-            // result.warnings
-            // result.errors
-        }
+            // This entire project exists because of the following line of code.  :|
+            // See https://github.com/google/closure-compiler/issues/1668
+            options.setRenamingPolicy(VariableRenamingPolicy.OFF, PropertyRenamingPolicy.OFF);
+            options.prettyPrint = true;
 
-        return compiler.toSource();
+            //List<SourceFile> builtinExterns = getBuiltinExterns(options.getEnvironment());
+            ArrayList<SourceFile> externs = new ArrayList<>();
+            ArrayList<SourceFile> source = new ArrayList<>();
+            source.add(SourceFile.fromCode(request.filename, request.source));
+            Result result = compiler.compile(externs, source, options);
+            if (!result.success) {
+                // result.warnings
+                // result.errors
+            }
 
-        //response.status(200);
-        //response.body(compiler.toSource());
-    });
-  }
+            res.type("application/json");
 
+            CompileResponse response = new CompileResponse();
+            response.source = compiler.toSource();
+            return response;
+            //return compiler.toSource();
+
+            //response.status(200);
+            //response.body(compiler.toSource());
+        }, JsonUtil.json());
+    }
 }

115dceaf737627a0c4f91934b0858ae0b4dac5aa
Chad Austin <chad@chadaustin.me>
2016-04-11 08:35:45 -0700

    style

--- stage/playground.js
+++ stage/playground.js
@@ -1,4 +1,46 @@
 (function() {
+  var _rts_current_exception = null;
+
+function _rts_set_exception(e) {
+    _rts_current_exception = e;
+}
+
+function _rts_clear_exception() {
+    _rts_current_exception = null;
+}
+
+function _rts_new_exception(name, baseException) {
+    // TODO: validate the name is a valid js identifier
+    // TODO: catch the eval exception in CSP contexts and try something simpler in that case
+    var ctor = new Function("name", "return function " + name + "(message, e) {\n  this.message = message;\n  this.buildStack(e || new Error);\n}\n")(name);
+
+    ctor.prototype.name = name;
+    ctor.prototype.toString = function() {
+        if (this.message === undefined) {
+            return this.name;
+        } else {
+            return this.name + ": " + this.message;
+        }
+    };
+    ctor.prototype.buildStack = function(e) {
+        var stack = e.stack;
+        if (stack !== undefined) {
+            this.stack = this.toString() + '\n' +
+                stack.replace(/^Error(:[^\n]*)?\n/, '');
+        }
+    };
+
+    ctor.throw = function(message, e) {
+        throw new ctor(message, e);
+    };
+
+    ctor.check = function(e) {
+        return e instanceof ctor;
+    };
+
+    return ctor;
+}
+;
   var $array_get;
   var $array_len;
   var $array_replicate;

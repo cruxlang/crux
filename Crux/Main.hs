34182c8e523eb0211b1cca14f6a278bac45aa084
Chad Austin <chad@chadaustin.me>
2016-11-11 17:40:39 -0800

    add 'crux compile' which compiles an individual file

--- Crux/Main.hs
+++ Crux/Main.hs
@@ -5,7 +5,7 @@ import qualified Crux.Gen as Gen
 import qualified Crux.JSBackend as JS
 import Crux.Module (loadProgramFromFile, loadRTSSource)
 import Crux.Prelude
-import Crux.Project (buildProject, buildProjectAndRunTests)
+import Crux.Project (buildProject, buildProjectAndRunTests, runJS)
 import qualified Data.Text as Text
 import Options.Applicative
 import Options.Applicative.Types (readerAsk)
@@ -15,9 +15,13 @@ import Data.List (isSuffixOf)
 
 data Command
     = VersionCommand
+    | CompileCommand FilePath
     | RunCommand FilePath
     | BuildCommand
     | TestCommand
+
+compileCommand :: Parser Command
+compileCommand = CompileCommand <$> argument str mempty
 
 runCommand :: Parser Command
 runCommand = RunCommand <$> argument str mempty
@@ -28,6 +32,7 @@ parseVersion = flag' VersionCommand (long "version" <> hidden)
 parseCommand :: Parser Command
 parseCommand = subparser $ mconcat
     [ command "build" $ pure BuildCommand `withInfo` "build project"
+    , command "compile" $ compileCommand `withInfo` "compile single file"
     , command "run" $ runCommand `withInfo` "run program file"
     , command "test" $ pure TestCommand `withInfo` "test project"
     , command "version" $ pure VersionCommand `withInfo` "print compiler version"
@@ -50,6 +55,17 @@ parseOptions = parseVersion <|> parseCommand <|> parseSingleScript
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc
 
+compileToJS :: FilePath -> IO Text
+compileToJS fn = do
+    rtsSource <- loadRTSSource
+    loadProgramFromFile fn >>= \case
+        Left err -> do
+            Error.printError stderr err
+            exitWith $ ExitFailure 1
+        Right program -> do
+            program'' <- Gen.generateProgram program
+            return $ JS.generateJS rtsSource program''
+
 main :: IO ()
 main = do
     cmd <- execParser $ info (helper <*> parseOptions) mempty
@@ -60,13 +76,9 @@ main = do
             buildProject
         TestCommand -> do
             buildProjectAndRunTests
+        CompileCommand fn -> do
+            js <- compileToJS fn
+            putStr $ Text.unpack js
         RunCommand fn -> do
-            rtsSource <- loadRTSSource
-            loadProgramFromFile fn >>= \case
-                Left err -> do
-                    Error.printError stderr err
-                    exitWith $ ExitFailure 1
-                Right program -> do
-                    program'' <- Gen.generateProgram program
-                    putStr $ Text.unpack $ JS.generateJS rtsSource program''
-                    exitWith ExitSuccess
+            js <- compileToJS fn
+            runJS $ Text.unpack js
